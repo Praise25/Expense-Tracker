25bcc5c859f83ddedcb39d6d9a24f848c24120a2
Praise25 <anenepraise1@gmail.com>
2022-09-17 18:55:34 +0100

    completed section 4

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,29 +1,20 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
-import { useState } from "react";
 
 const ExpenseItem = (props) => {
-  props = {...props.expenseDetails};
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    setTitle("Updated!");
-    console.log(title);
-  }
-
+  props = { ...props.expenseDetails };
   return (
     <Card className="expense-item">
       <div>
         <ExpenseDate date={props.date} />
       </div>
       <div className="expense-item-description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item-price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,16 +1,25 @@
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
 import "./Expenses.css";
+import { useState } from "react";
 
 const Expenses = (props) => {
-  return(
+  const [newYear, setNewYear] = useState("2020");
+
+  const changeYearHandler = (year) => {
+    setNewYear(year);
+  };
+
+  return (
     <Card className="expenses">
-      <ExpenseItem expenseDetails={props.expenseData[0]}/>
-      <ExpenseItem expenseDetails={props.expenseData[1]}/>
-      <ExpenseItem expenseDetails={props.expenseData[2]}/>
-      <ExpenseItem expenseDetails={props.expenseData[3]}/>
+      <ExpensesFilter selected={newYear} onChangeYear={changeYearHandler} />
+      <ExpenseItem expenseDetails={props.expenseData[0]} />
+      <ExpenseItem expenseDetails={props.expenseData[1]} />
+      <ExpenseItem expenseDetails={props.expenseData[2]} />
+      <ExpenseItem expenseDetails={props.expenseData[3]} />
     </Card>
   );
-}
+};
 
 export default Expenses;
